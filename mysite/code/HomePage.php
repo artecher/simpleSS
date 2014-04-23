c2b32d50b29566e1d7819db99df8523957fcd7f8
ethan <ethanleenz@gmail.com>
2014-04-23 16:50:31 +1200

    Local commit to creat master branch

---
+++ mysite/code/HomePage.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: wison
+ * Date: 2014/4/21
+ * Time: 18:08
+ */
+class HomePage extends Page{
+
+}
+class HomePage_Controller extends Page_Controller{
+
+}
